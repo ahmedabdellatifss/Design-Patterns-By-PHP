93d145c051aaf6ef2b546ba7af1f781207c9bb2b
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2022-06-09 15:22:48 +0200

    Pool Pattern In Creational Design Pattern

---
+++ Creational/Pool/CarPool.php
@@ -0,0 +1,40 @@
+<?php
+
+    namespace Creational\Pool;
+
+    class CarPool 
+    {
+        private $freeCars = [];
+        private $busyCars = [];
+
+        public function rentCar()
+        {
+            if(count($this->freeCars) == 0 ){
+                $car = new Car();
+            }else{
+                $car = array_pop($this->freeCars);
+            } 
+            $this->busyCars[spl_object_hash($car)] = $car;
+            return $car;
+        }
+
+        public function freeCar (Car $car)
+        {
+            $carId = spl_object_hash($car);
+            if(isset($this->busyCars[$carId])){
+                unset($this->busyCars[$carId]);
+                $this->freeCars[$carId] = $car;
+            }
+
+        }
+
+        public function getReport()
+        {
+            return count($this->freeCars) + count($this->busyCars);
+        }
+
+        public  function  getFreeCount() {
+            return count ($this->freeCars);
+        }
+
+    }
