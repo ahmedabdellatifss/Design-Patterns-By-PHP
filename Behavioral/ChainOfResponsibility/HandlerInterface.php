49be501abf491dd689624de5ede682a5a3784164
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2022-08-03 19:57:51 +0200

    Chain of Responsiblity - Behavioral Design Pattern

---
+++ Behavioral/ChainOfResponsibility/HandlerInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+    namespace Behavioral\ChainOfResponsibility;
+    
+    interface HandlerInterface
+    {
+        public function setNext(HandlerInterface $handler);
+        public function handle(Request $request);
+    }
